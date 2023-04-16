84ac79f9bf83d65ed7a4cee11bbcdca86fe5450a
Chau Ka Leung <lacusleung012@gmail.com>
2023-04-16 19:51:41 +0800

    update

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -1,5 +1,7 @@
 import Input from "@/components/input";
+import axios from "axios";
 import { useCallback, useState } from "react";
+import { signIn } from 'next-auth/react';
 
 const Auth = () => {
     const [ email, setEmail] = useState('');
@@ -11,6 +13,22 @@ const Auth = () => {
     const toggleVariant = useCallback(() => {
         setVarinant((currentVariant) => currentVariant === 'login' ? 'register' : 'login')
     }, [])
+
+    const register = useCallback(async () => {
+        try {
+            await axios.post('/api/register', {email, name, password})
+        } catch (error) {
+            console.log(error);
+        }
+    }, [email, name, password])
+
+    const login = useCallback(async () => {
+        try {
+            await signIn('credentials', { email, password, redirect: false, callbackUrl: '/' })
+        } catch (error) {
+            console.log(error);
+        }
+    }, [email, password])
 
     return (
         <div className="relative h-full w-full bg-[url('/images/hero.jpg')] bg-no-repeat bg-center bg-fixed bg-cover">
@@ -28,7 +46,7 @@ const Auth = () => {
                             <Input id="email" onChange={(event:any) => setEmail(event.target.value)} value={email} label="email" type="email" />
                             <Input id="password" onChange={(event:any) => setPassword(event.target.value)} value={password} label="Password" type="password" />
                         </div>
-                        <button className="bg-red-600 py-3 text-white rounded-md w-full mt-10 hover:bg-red-300 translate">
+                        <button onClick={varinant === 'login'? login : register} className="bg-red-600 py-3 text-white rounded-md w-full mt-10 hover:bg-red-300 translate">
                             {varinant === 'login'? 'Login' : 'Sign Up'}
                         </button>
                         <p className="text-neutral-500 mt-12">
